2906eb0071a2539a005a9be98008e72ab043617b
Alexey <kodukhov.ad@phystech.edu>
2018-10-26 15:33:55 +0300

    task_4 done

--- task_4/monte.c
+++ task_4/monte.c
@@ -13,21 +13,26 @@
 #include <semaphore.h>
 #include <sys/shm.h>
 #include <string.h>
+#include <time.h>
 
 int N;          //amount of streams
-//pthread_t* tid;
-pthread_t tid[2];
+pthread_t* tid;
+//pthread_t tid[2];
 float* res;
 int NUM = 0;
+int coun = 0;
+sem_t* sem;
+int max = 5000000;
+#define BILLION 1000000000L;
 
 void* thread_stuff(void *arg)
 {
 	int i = 0;
 	int k = 0;
-	int max = 1000000;
 	float integ = 0;
 	int n = 0;
 	float x, y, val;
+	sem = sem_open("NAME",0);
 	if(NUM<N){
 		n = NUM;
 		NUM++;
@@ -37,29 +42,42 @@ void* thread_stuff(void *arg)
 		printf("ERROR In TREAD_STUFF\n");
 		return NULL;
 	}
-	for(i=0;i<max;i++){
-		x = (float)rand()/RAND_MAX;
-		y = (float)rand()/RAND_MAX;
+	for(i=0;i<max/N;i++){
+		x = (float)rand()*2/RAND_MAX;
+		y = (float)rand()*4/RAND_MAX;
 		val = x*x;
 		if(y<=val){
 			k++;		
 		}
 	}
-	float cyk = 1.0/max;
-	integ = k*cyk;
+	float del = 1.0/max;
+	integ = k*del*8*N;
 	//printf("{%f}\n", integ);
 	res[n]=integ;
+	coun++;
+	if(coun==N){
+		sem_post(sem);
+	}
 	return NULL;
 }
 
 int main(void){
 	printf("ENTER THE NUMBER OF STREAMS:\n");
 	scanf("%d", &N);
-	//tid = malloc(sizeof(pthread_t)*N);
+	tid = malloc(sizeof(pthread_t)*N);
 	res = malloc(sizeof(float)*N);
 	int i = 0;
 	NUM = 0;
 	int err;
+
+	struct timespec start, stop;
+        double accum;
+
+	if( clock_gettime( CLOCK_MONOTONIC, &start) == -1 ) {
+                perror( "clock gettime" );
+                return EXIT_FAILURE;
+	}
+
 	while(i < N) {
         	err = pthread_create(&(tid[i]), NULL, &thread_stuff, NULL);
         	if (err != 0){
@@ -70,15 +88,28 @@ int main(void){
 		}
 		i++;
 	}
-	sleep(4);
+	sem = sem_open("NAME",O_CREAT,0776,0);
+	sem_wait(sem);
+	//sleep(4);
 	float aver = 0;
 	for(i=0;i<N;i++){
 		//printf("[%f]", res[i]);
 		aver = aver + res[i];
 	}
 	aver = aver/N;
-	printf("integrate(x^2dx),[0;1]=%f\n", aver);
+	printf("integrate(x^2dx),[0;2]=%f\n", aver);
+
+	if( clock_gettime( CLOCK_MONOTONIC, &stop) == -1 ) {
+               		 perror( "clock gettime" );
+        	        return EXIT_FAILURE;
+	}
+	
+	accum = ( stop.tv_sec - start.tv_sec )
+             + (double)( stop.tv_nsec - start.tv_nsec )
+	/                (double)BILLION;
+       		 accum = accum - accum - accum;
+	printf( "time=%lf seconds\n", accum );
 	free(res);
-	//free(tid);
+	free(tid);
 	return 0;
 }
